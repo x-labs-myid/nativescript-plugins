1553bc01a168f8a21b7ac2f1da882b9abbb9ab97
Kang Cahya <cahyadyazin@yahoo.com>
2025-08-01 05:50:40 +0700

    Initial commit

---
+++ tools/workspace-scripts.js
@@ -0,0 +1,66 @@
+module.exports = {
+  message: 'NativeScript Plugins ~ made with ❤️  Choose a command to start...',
+  pageSize: 32,
+  scripts: {
+    default: 'nps-i',
+    nx: {
+      script: 'nx',
+      description: 'Execute any command with the @nrwl/cli',
+    },
+    format: {
+      script: 'nx format:write',
+      description: 'Format source code of the entire workspace (auto-run on precommit hook)',
+    },
+    '🔧': {
+      script: `npx cowsay "NativeScript plugin demos make developers 😊"`,
+      description: '_____________  Apps to demo plugins with  _____________',
+    },
+    // demos
+    apps: {
+      '...Vanilla...': {
+        script: `npx cowsay "Nothing wrong with vanilla 🍦"`,
+        description: ` 🔻 Vanilla`,
+      },
+      demo: {
+        clean: {
+          script: 'nx clean demo',
+          description: '⚆  Clean  🧹',
+        },
+        ios: {
+          script: 'nx debug demo ios',
+          description: '⚆  Run iOS  ',
+        },
+        android: {
+          script: 'nx debug demo android',
+          description: '⚆  Run Android  🤖',
+        },
+      },
+    },
+    '⚙️': {
+      script: `npx cowsay "@nativescript/* packages will keep your ⚙️ cranking"`,
+      description: '_____________  @nativescript/*  _____________',
+    },
+    // packages
+    // build output is always in dist/packages
+    '@nativescript': {
+      'build-all': {
+        script: 'nx run-many --target=build.all --all',
+        description: 'Build all packages',
+      },
+    },
+    '⚡': {
+      script: `npx cowsay "Focus only on source you care about for efficiency ⚡"`,
+      description: '_____________  Focus (VS Code supported)  _____________',
+    },
+    focus: {
+      reset: {
+        script: 'nx g @nativescript/plugin-tools:focus-packages',
+        description: 'Reset Focus',
+      },
+    },
+    '.....................': {
+      script: `npx cowsay "That's all for now folks ~"`,
+      description: '.....................',
+    },
+  },
+};
